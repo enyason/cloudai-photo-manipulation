3a24a4f7ee910016d759327e3c3646cb9b1aaf76
enyamest <emmanuelenya@meltwater.org>
2023-12-03 14:07:48 +0100

    extract secretes from env

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -1,4 +1,4 @@
-export const API_KEY = ""
-export const IMG_BB_API_KEY = ""
+export const API_KEY = import.meta.env.REPLICATE_API_KEY
+export const IMG_BB_API_KEY = import.meta.env.IMG_BB_API_KEY
 export const BASE_URL = 'https://api.replicate.com/v1/'
 export const PROXY_SERVER_URL = "https://proxy-server-407001.ue.r.appspot.com/"
